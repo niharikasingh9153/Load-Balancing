7064c718a810814a2e017bb095041b7cfd5c10c1
niharikasingh9153 <singhniharika9153@gmail.com>
2025-11-06 16:58:30 +0530

    Initialize Payment Service application with Spring Boot

---
+++ PaymentServiceApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
+}
